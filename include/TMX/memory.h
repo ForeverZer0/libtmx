44386d64d35038f0edff96e71c7caea3a494bcc6
ForeverZer0 <efreed09@gmail.com>
2023-03-20 23:25:51 -0400

    Implemented allocators and user callbacks

--- include/TMX/memory.h
+++ include/TMX/memory.h
@@ -12,7 +12,7 @@
  * @param size The number of bytes to allocate.
  * @param user The user-defined value assigned when the callback was set.
  *
- * @return A pointer to the allocated memory.
+ * @return A pointer to the allocated memory, or @c NULL if the request fails.
  */
 typedef void *(*TMXmallocfunc)(size_t size, TMXuserptr user);
 
@@ -32,11 +32,11 @@ typedef void *(*TMXreallocfunc)(void *previous, size_t newSize, TMXuserptr user)
 /**
  * @brief Prototype for a user-defined @c calloc function.
  *
- * @param elemCount
- * @param elemSize
+ * @param elemCount The number of elements to be allocated.
+ * @param elemSize The size of a single element.
  * @param user The user-defined value assigned when the callback was set.
  *
- * @return
+ * @return A pointer to the allocated memory, or @c NULL if the request fails.
  */
 typedef void *(*TMXcallocfunc)(size_t elemCount, size_t elemSize, TMXuserptr user);
 
@@ -60,18 +60,51 @@ typedef struct TMXallocator
     TMXfreefunc free;       /** The user-defined `free` function. */
 } TMXallocator;
 
-void tmxSetAllocator(TMXallocator allocator);
+/**
+ * @brief Assigns user-defined functions for allocating and freeing memory.
+ *
+ * @param allocator A structure containing the function pointers for memory allocation. All fields must be assigned.
+ * @param user A user-defined pointer that will be supplied with each call.
+ *
+ * @return @ref TMX_TRUE on success, otherwise @ref TMX_FALSE.
+ */
+TMXbool tmxSetAllocator(TMXallocator allocator, TMXuserptr user);
 
+/**
+ * @brief Allocates the requested memory and returns a pointer to it.
+ * @param size The size of the memory block, in bytes.
+ * @return A pointer to the allocated memory, or @c NULL if the request fails.
+ */
 void *tmxMalloc(size_t size);
 
+/**
+ * @brief Attempts to resize the memory block pointed to by @a previous that was previously allocated with a call to @ref tmxMalloc or @ref
+ * tmxCalloc.
+ *
+ * @param previous The pointer to a memory block previously allocated with @ref tmxMalloc, @ref tmxCalloc or @ref tmxRealloc to be
+ * reallocated. If this is @c NULL, a new block is allocated and a pointer to it is returned by the function.
+ * @param newSize The new size for the memory block, in bytes. If it is @c 0 and @a previous points to an existing block of memory, the
+ * memory block pointed by @a previous is deallocated and a @c NULL pointer is returned.
+ *
+ * @return A pointer to the newly allocated memory, or @c NULL if the request fails.
+ */
 void *tmxRealloc(void *previous, size_t newSize);
 
+/**
+ * @brief Alocates the requested memory and returns a pointer to it, with all memory initialized to zero.
+ *
+ * @param elemCount The number of elements to be allocated.
+ * @param elemSize The size of a single element.
+ *
+ * @return A pointer to the allocated memory, or @c NULL if the request fails.
+ */
 void *tmxCalloc(size_t elemCount, size_t elemSize);
 
+/**
+ * @brief Deallocates the memory previously allocated by a call to @ref tmxMalloc, @ref tmxCalloc, or @ref tmxRealloc
+ * @param memory Pointer to a memory block previously allocated with @ref tmxMalloc, @ref tmxCalloc, or @ref tmxRealloc to be deallocated.
+ * If a null pointer is passed as argument, no action occurs.
+ */
 void tmxFree(void *memory);
-
-#if TMX_DEBUG
-
-#endif
 
 #endif /* TMX_MEMORY_H */
--- src/memory.c
+++ src/memory.c
@@ -1,32 +1,109 @@
 
-#include <stddef.h>
+#include "TMX/memory.h"
+#include "TMX/error.h"
+#include "utils.h"
 #include <stdlib.h>
 
-// struct TMXmemorypool
-// {
+#ifdef TMX_DEBUG
+static size_t allocationCount;
+static size_t deallocationCount;
+#endif
 
-// }
-
-void *
-tmxMalloc(size_t size)
+static void *
+tmxMallocImpl(size_t size, TMXuserptr user)
 {
+    TMX_UNUSED(user);
     return malloc(size);
 }
 
-void *
-tmxRealloc(void *previous, size_t newSize)
+static void *
+tmxReallocImpl(void *previous, size_t newSize, TMXuserptr user)
 {
+    TMX_UNUSED(user);
     return realloc(previous, newSize);
 }
 
-void *
-tmxCalloc(size_t numElem, size_t elemSize)
+static void *
+tmxCallocImpl(size_t elemCount, size_t elemSize, TMXuserptr user)
 {
-    return calloc(numElem, elemSize);
+    TMX_UNUSED(user);
+    return calloc(elemCount, elemSize);
 }
 
-void
-tmxFree(void *memory)
+static void
+tmxFreeImpl(void *memory, TMXuserptr user)
 {
+    TMX_UNUSED(user);
     free(memory);
 }
+
+static TMXallocator memoryPool = {tmxMallocImpl, tmxReallocImpl, tmxCallocImpl, tmxFreeImpl};
+static TMXuserptr userPtr;
+
+inline void *
+tmxMalloc(size_t size)
+{
+    void *ptr = memoryPool.malloc(size, userPtr);
+    if (!ptr)
+        tmxError(TMX_ERR_MEMORY);
+    return ptr;
+}
+
+inline void *
+tmxRealloc(void *previous, size_t newSize)
+{
+#ifdef TMX_DEBUG
+    if (!previous)
+        allocationCount++;
+    else if (!newSize)
+        deallocationCount++;
+#endif
+
+    void *ptr = memoryPool.realloc(previous, newSize, userPtr);
+    if (!ptr)
+        tmxError(TMX_ERR_MEMORY);
+    return ptr;
+}
+
+inline void *
+tmxCalloc(size_t elemCount, size_t elemSize)
+{
+#ifdef TMX_DEBUG
+    allocationCount++;
+#endif
+    void *ptr = memoryPool.calloc(elemCount, elemSize, userPtr);
+    if (!ptr)
+        tmxError(TMX_ERR_MEMORY);
+    return ptr;
+}
+
+inline void
+tmxFree(void *memory)
+{
+#ifdef TMX_DEBUG
+    if (memory)
+        deallocationCount++;
+#endif
+    memoryPool.free(memory, userPtr);
+}
+
+#ifdef TMX_DEBUG
+
+#include <stdio.h>
+
+#define BLUE   "\033[34m"
+#define YELLOW "\033[33m"
+#define GREEN  "\033[32m"
+#define RED    "\033[31m"
+#define BRIGHT "\033[1m"
+#define RESET  "\033[0m"
+
+void
+tmxMemoryLeakCheck(void)
+{
+    printf("\n" BLUE ":: " RESET BRIGHT "Leak Check\n" RESET);
+    printf(YELLOW " -> " RESET "Allocations:    %5zu\n", allocationCount);
+    printf(YELLOW " -> " RESET "Deallocations:  %5zu\n", deallocationCount);
+    printf(YELLOW " -> " RESET "Result:          " BRIGHT "%s\n\n" RESET, allocationCount == deallocationCount ? GREEN "PASS" : RED "FAIL");
+}
+#endif
